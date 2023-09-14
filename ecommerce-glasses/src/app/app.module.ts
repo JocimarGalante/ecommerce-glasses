719a3d392fb60f63c9cd38559df94c17f4b418c2
JocimarGalante <jocimargalante@gmail.com>
2023-09-14 17:56:55 -0300

    Adicionado mdboostrap

--- ecommerce-glasses/src/app/app.module.ts
+++ ecommerce-glasses/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './componentes/menu/menu.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { HeadersComponent } from './componentes/headers/headers.component';
+import { ScrollComponent } from './componentes/scroll/scroll.component';
+import { CarouselComponent } from './componentes/carousel/carousel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    FooterComponent,
+    HeadersComponent,
+    ScrollComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule,
